45b03705815a81b8344fd09ae6bf478b2da6ba17
henteko <henteko07@gmail.com>
2013-01-16 15:57:53 +0900

    pixivオブジェクトを取得出来た

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-$(function(){
+var script = function(){
+    console.log(pixiv.user);
+    jQuery('<div>', {
+        id:'toMoyashipan', 
+        'data-pixivuser':JSON.stringify(pixiv.user), 
+        'data-pixivcontext':JSON.stringify(pixiv.context), 
+        style:'display:none;'
+    }).appendTo(jQuery('body'));
+};
+
+$(document).ready(function () {
+    setTimeout (function() {
+        location.href = 'javascript:('+script.toString()+')()';
+    }, 100);
 });
+
