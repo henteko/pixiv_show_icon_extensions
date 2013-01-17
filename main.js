10d5348393fec43afa7138cb449df39ae48c189a
henteko <henteko07@gmail.com>
2013-01-17 17:00:23 +0900

    ユーザーの名前とプロフィールページリンク追加

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
                     $.get(url, function(data) {
                         if(data.success == true) {
                             //成功時
-                            icon_data = [{user_icon_url: img_url}];
+                            icon_data = [{user_icon_url: img_url, user_id: pixiv_user.id}];
                             setIcon(icon_data);
                             $("div.rating").unbind();
                         }
@@ -56,11 +56,17 @@ function setIcon(data) {
         class: "vote_user"
     });
     $.each(data, function(id) {
+        var $a = $("<a>", {
+            href: "http://www.pixiv.net/member.php?id=" + data[id].user_id,
+            class: "ui-tooltip",
+            "data-tooltip": data[id].user_name ? data[id].user_name : "名無しさん"
+        });
         var $img = $("<img>",{
             id: "rating_user_icon",
             src: data[id].user_icon_url
         });
-        $vote_users.append($img);
+        $a.append($img);
+        $vote_users.append($a);
     });
     $score.append($vote_users);
 }
